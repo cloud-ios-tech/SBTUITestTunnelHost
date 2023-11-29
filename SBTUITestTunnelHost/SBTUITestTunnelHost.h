feb8ea983a72d8fcc68195d35dfdebff6598859c
Денис Строганов <d.stroganov@vk.team>
2023-11-29 15:15:58 +0300

    Carthage support

---
+++ SBTUITestTunnelHost/SBTUITestTunnelHost.h
@@ -0,0 +1,16 @@
+//
+//  SBTUITestTunnelHost.h
+//  SBTUITestTunnelHost
+//
+//  Created by Denis Stroganov on 30.11.2023.
+//
+
+#ifndef SBTUITestTunnelHost_h
+#define SBTUITestTunnelHost_h
+
+#import <SBTUITestTunnelHost/SBTUITunneledHost.h>
+#import <SBTUITestTunnelHost/SBTUITunneledHostMouseClick.h>
+#import <SBTUITestTunnelHost/SBTUITunneledHostMouseDrag.h>
+#import <SBTUITestTunnelHost/XCTestCase+HostExtension.h>
+
+#endif /* SBTUITestTunnelHost_h */
